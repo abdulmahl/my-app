754b5d9d49b2db9d5917be165ce43d8221d7f635
abdulmahl <mahalanguma87@gmail.com>
2023-11-11 10:25:27 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+
 function App() {
+  const course = "Half stack app dev";
+  const part1 = {
+    name: "React Fundamentals",
+    exercises: 10,
+  }
+
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  }
+
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  }
+    
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>{course}</h1>
+
+      <div className='userCard'>
+        <img
+          src={user.imageUrl}
+          alt={"photo of " + user.name} 
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+          />
+        <h2>{user.name}</h2>
+      </div>
+
+      <ul>
+        <li>{part1.name}</li> 
+        <li>{part1.exercises}</li>
+      </ul>
+
+      <ol>
+        <li>{part2.name}</li>
+        <li>{part2.exercises}</li>
+      </ol>
+
+      <ul>
+        <li>{part3.name}</li>
+        <li>{part3.exercises}</li>
+      </ul>
+
     </div>
+    
   );
 }
 
--- src/myComponents/App.js
+++ src/myComponents/App.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
+
+//? How to create and nest components.
 function MyButton() {
     return (
-        <button type="submit">I am a button</button>
+        <button type="submit">
+            I am a button
+        </button>
     );
 }
 
@@ -8,7 +13,130 @@ export default function MyApp() {
     return (
         <div>
             <h1>Welcome to my app!</h1>
-            <MyButton/>
+            <MyButton />
         </div>
     );
 }
+
+//? How to add markup and styles.
+function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Hello there.<br />How do you do?</p>
+            {/* Adding markups and styles */}
+            <img className="avatar" />
+        </>
+        
+    );
+}
+
+
+//?  Displaying data 
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+export default function profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img 
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
+
+//? Rendering conditions
+let content;
+if(isLoggein) {
+    content = <Adminpanel />
+} else {
+    content = <Loginform />
+}
+
+return (
+    <div>
+        content
+    </div>
+);
+
+//? Using the ? operator instead of if stmt.
+
+<div>
+    {isLoggedin ? ( 
+        <Adminpanel />
+    ) : ( 
+        <Loginform />
+    )}
+</div>
+
+//?When you don't have an else stmt you can use the and condition.
+return (
+    <div>
+        {isLoggedin && <Adminpanel />}
+    </div>
+)
+
+//? Rendering List.
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+];
+
+//? Inside the products component use the map() function to turn
+//? the array to aanarray of <li></li>.
+const listItems = products.map(product => 
+    <li key={product.id}>
+        {product.title}
+        style={{
+            color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}
+    </li>
+);
+
+return (
+    <ul>
+        {listItems}
+    </ul>
+);
+
+//? Responding to events. declaring event handler functions.
+function MyButton() {
+    function handleClick() {
+        alert('You clicked me!');
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Click me
+        </button>
+    );
+}
+
+//? Updating the screen.
+function MyButton() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Clicked {count} times
+        </button>
+    );
+}
+
+//? Using hooks.
+
